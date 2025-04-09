6df0ef2d55c2851c8a192ac0e1f2b19df1a16e47
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2025-04-09 19:54:06 +0700

    add comment demo

--- src/test/java/com/anhtester/Bai5_Locators/DemoHTMLAttribute.java
+++ src/test/java/com/anhtester/Bai5_Locators/DemoHTMLAttribute.java
@@ -10,27 +10,27 @@ public class DemoHTMLAttribute {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
 
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize(); //maximize để phóng to màn hình hết cỡ
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //xét thời gian tối đa là 10s để tìm kiếm Element
 
-        driver.get("https://crm.anhtester.com/admin/authentication");
+        driver.get("https://crm.anhtester.com/admin/authentication"); // để mở 1 URL
 
-        driver.findElement(By.id("email")).sendKeys("admin@example.com");
+        driver.findElement(By.id("email")).sendKeys("admin@example.com"); //id
 
-        driver.findElement(By.name("password")).sendKeys("123456");
+        driver.findElement(By.name("password")).sendKeys("123456"); //name
 
-//        driver.findElement(By.linkText("Forgot Password?")).click();
+//        driver.findElement(By.linkText("Forgot Password?")).click(); //LinkText
 
-//        driver.findElement(By.partialLinkText("Forgot")).click();
+//        driver.findElement(By.partialLinkText("Forgot")).click(); //PartialLinkText
 
-//        driver.findElement(By.tagName("button")).click();
+//        driver.findElement(By.tagName("button")).click(); //TagName
 
-//        driver.findElement(By.className("btn-primary")).click();
+//        driver.findElement(By.className("btn-primary")).click(); //ClassName
 
-        driver.findElement(By.xpath("//button")).click();
+        driver.findElement(By.xpath("//button")).click();  //xPath
 
-        Thread.sleep(4000);
+        Thread.sleep(4000); // dừng màn hình 4s trước khi đóng trình duyệt (thời gian nghỉ)
 
-        driver.quit();
+        driver.quit(); //tắt trình duyệt
     }
 }
