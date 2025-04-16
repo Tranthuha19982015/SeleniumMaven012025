af02d236567425171b505356952cb0b92d568084
Tranthuha19982015 <Tranthuha19982015@gmail.com>
2025-04-16 16:54:03 +0700

    DemoXpath1

--- src/test/java/com/anhtester/Bai5_Locators/DemoXPathAbsolute.java
+++ src/test/java/com/anhtester/Bai5_Locators/DemoXPathAbsolute.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 public class DemoXPathAbsolute {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
@@ -16,5 +16,14 @@ public class DemoXPathAbsolute {
         driver.get("https://crm.anhtester.com/admin/authentication");
 
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/form/div[1]/input")).sendKeys("admin@example.com");
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/form/div[2]/input")).sendKeys("123456");
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/form/div[4]/button")).click();
+
+//        driver.findElement(By.xpath("/html/body/aside/ul/li[3]/a/span")).click();
+//        driver.findElement(By.xpath("/html/body/div[3]/div/div/div/div[1]/a[1]")).click();
+        driver.findElement(By.xpath("//ul[@id='side-menu']/li"));
+
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
